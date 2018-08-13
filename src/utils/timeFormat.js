813c1c462e597cf1940b655ced063d3c7221f6fe
FredaHu <fredyqiu@hotmail.com>
2018-08-13 11:32:55 +0800

    fix-time-format

--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -2,7 +2,7 @@ import { dateFormat } from 'vux'
 const getNormalTime = dateFormat(new Date(), 'YYYY-MM-DD')
   // dateFormat(new Date(), 'YYYY-MM-DD HH:mm:ss')
 const msToDate = function (ms){
-  return dateFormat(new Date(ms), 'YYYY-MM-DD HH') + ':00'
+  return dateFormat(new Date(ms), 'YYYY-MM-DD HH:mm')
 }
 
 export {msToDate}
